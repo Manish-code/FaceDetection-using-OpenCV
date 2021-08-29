5e5b2d3401e09e6e8d09d57bedd0058532c52a87
Manish Narayan <manish.amhs@gmail.com>
2021-08-30 05:10:42 +0530

    eye detector commented

--- Smile Detector/smile_detector.py
+++ Smile Detector/smile_detector.py
@@ -3,7 +3,7 @@ import cv2
 # face and smile classifiers
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
-
+#eyes_detector = cv2.CascadeClassifier('haarcascade_eye.xml')
 # Grab webcam feed
 webcam = cv2.VideoCapture(0)
 
@@ -35,6 +35,18 @@ while True:
         face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
 
         smiles = smile_detector.detectMultiScale(face_grayscale, scaleFactor=1.7, minNeighbors=20)
+       # eyes = eyes_detector.detectMultiScale(face_grayscale, scaleFactor=1.3, minNeighbors=10)
+
+    
+        # # Run smile detection with each of the face
+        # for (x_, y_, w_, h_) in smiles:
+        #      # Draw rectangles around the smile
+        #         cv2.rectangle(the_face, (x_, y_), (x_+w_, y_+h_), (100, 200, 50), 2)
+
+        #   # Run smile detection with each of the face
+        # for (x__, y__, w__, h__) in eyes:
+        #      # Draw rectangles around the smile
+        #         cv2.rectangle(the_face, (x__, y__), (x__+w__, y__+h__), (100, 200, 50), 2)       
 
         #label ths face as smiling
         if len(smiles) > 0:
@@ -43,6 +55,11 @@ while True:
 
      # Show the current frame
     cv2.imshow('Why So Serious?', frame)
+
+   
+       
+
+    
 
      #stop if Q key is pressed
     key = cv2.waitKey(1)
